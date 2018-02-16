b5935386d0ffadecfbc7c35f32afebd493716cce
Jessica Yang <jessica.yang@cca.edu>
2018-02-16 08:33:45 -0800

    Create water-arrays.js

---
+++ water-arrays.js
@@ -0,0 +1,34 @@
+var x = [];
+var y = [];
+var ySpeed = []
+var s = []
+
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB)
+    for (var index = 0; index < 10; index++) {
+    x[index] = 230;
+    y[index] = 220;
+    ySpeed[index] = random(1, 5);
+    s[index] = random(8, 15);
+  }
+}
+  
+function draw() {
+  background(0);
+  noStroke();
+
+  // draw pipe
+  rect(0, 200, 230, 20);
+  
+  for (var index = 0; index < 50; index++) {
+    ellipse(x[index], y[index], s[index]);
+    y[index] = y[index] + ySpeed[index];
+
+
+    if (y[index] > 450) {
+      // reset
+      y[index] = 220;
+    }
+}
+}
