e426d937ef2c6121c873736e013a4f7af6737678
Jessica Yang <jessica.yang@cca.edu>
2018-02-16 16:50:31 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -5,12 +5,12 @@ var color = [];
 
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB)
+  colorMode(RGB)
   for (var index = 0; index < 50; index++) {
     x[index] = random(100, 300);
     y[index] = random(100, 250);
     d[index] = random(30, 100);
-    color[index] = random(255, 255, 255);
+    color[index] = color(random(255), random(255), random(255));
 
   }
 }
@@ -21,10 +21,13 @@ function draw() {
 
   // draw two ellipses
   for (var index = 0; index < 50; index++) {
+    fill(color[index]);
     ellipse(x[index], y[index], d[index]);
-    if (random() < 0.03) {
-      d[index] = d[index];
-    }
-}
   
+  if (random() < 50) {
+    // random diameter between 10 and 400
+    d[index] = random(10, 300);
+  }
+  
+}
 }
